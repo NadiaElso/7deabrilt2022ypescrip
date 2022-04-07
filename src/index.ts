b75dd84de9492f92bf9f30de783104314abd5e60
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2022-04-07 20:37:29 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+console.log("Hola mundo");
+ let saludarusuario= document.getElementById("saludar");
+ function holausuario() {
+  console.log("Hola usuario");
+ 
+}
+
+ saludarusuario?.addEventListener("clik", holausuario);
+
+ 
+
+
+
+ holausuario();
